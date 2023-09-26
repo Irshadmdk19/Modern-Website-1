12032be99a47563f4c7ae9be95cbdf2c5369df02
irshadmdk19 <muirshad.ai20@sahyadri.edu.in>
2023-09-26 21:43:55 +0530

    Animated navebar, frontpage and herofooter

--- myjs.js
+++ myjs.js
@@ -2,3 +2,22 @@ const scroll = new LocomotiveScroll({
     el: document.querySelector('.main'),
     smooth: true
 });
+
+// #First page animating
+function firstPageAnim(){
+    var t1=gsap.timeline()
+    t1.from('.nav',{
+        y:'-20',opacity:0, duration:1.5, ease: Expo.easeInOut
+    })
+    .to('.boundingelem',{
+        y:0, duration:1.5, 
+        stagger:0.2,
+         ease: Expo.easeInOut
+    })
+    .from('.herofooter',{
+        y:'-20',opacity:0, duration:1.5,
+        delay:-1, ease: Expo.easeInOut
+    })
+
+}
+firstPageAnim()
